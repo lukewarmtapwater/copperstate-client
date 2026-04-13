cb05a6d93a50ece33ecade7181982b39e026f39f
Umair <umairsaeed01x@gmail.com>
2026-04-14 00:28:45 +0500

    minor change

--- src/pages/app/sections/no-access.jsx
+++ src/pages/app/sections/no-access.jsx
@@ -1,6 +1,6 @@
 function NoAccess() {
   return (
-    <div className="m-8">
+    <div className="m-6">
       <h3>No Access</h3>
       <p className="mt-1">
         You haven't been assigned a role yet by a manager or higher.
